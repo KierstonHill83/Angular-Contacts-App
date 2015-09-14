7d1318525f7e3b771f55df6b4dffebf12731108b
KierstonHill83 <khill1218@gmail.com>
2015-09-14 10:24:26 -0600

    Added a form for the contacts

--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
-var app = angular.module('myApp', []);
+angular.module('myApp', []);
 
 
-app.controller('firstController', ['$scope', function($scope) {
-}]);
+var firstControllerFunction = function($scope) {
+
+  var harry = {
+    'name': 'Harry Potter',
+    'email': 'harry@hogwarts.com',
+    'phone': '(232) 383-5647'
+  };
+  var edward = {
+    'name': 'Edward Scissorhands',
+    'email': 'ed@hotmail.com',
+    'phone': '(413) 987-5739'
+  };
+  var willy = {
+    'name': 'Willy Wonka',
+    'email': 'willy@gmail.com',
+    'phone': '(221) 938-4525'
+  };
+  var arya = {
+    'name': 'Arya Stark',
+    'email': 'arya@yahoo.com',
+    'phone': '(165) 437-0826'
+  };
+
+  $scope.people = [harry, edward, willy, arya];
+
+  $scope.addContact = function() {
+    var contact = {
+      'name': $scope.name,
+      'email': $scope.email,
+      'phone': $scope.phone,
+    };
+
+  $scope.people.push(contact);
+  };
+
+};
+
+
+angular.module('myApp').controller('firstController', ['$scope', firstControllerFunction]);
